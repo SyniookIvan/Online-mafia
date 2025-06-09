e0ee639c4efae97681b783cd827186361f5d7d04
Ivan <IvanSyniook@gmail.com>
2025-06-09 15:39:41 +0300

    Add button in product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,9 +1,21 @@
-export default function ProductPage({ params }: { params: { id: string } }) {
+import { Button } from "@/shared/components";
+
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const resolverParams = await params;
+  const id = Number(resolverParams.id);
+
   return (
-    <div className="flex  justify-center">
-      <h1>
-        <b>Hello</b> it`s product page №{params.id}
-      </h1>
+    <div className="flex min-h-screen justify-center items-center">
+      <div className="flex flex-col items-center space-y-4">
+        <h1 className="text-2xl font-bold">
+          Hello, it&apos;s product page №{id}
+        </h1>
+        <Button variant="outline">Тицьни мене</Button>
+      </div>
     </div>
   );
 }
