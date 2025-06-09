5c85b6f9febaed64893e17629159a3006c401fd4
Ivan <IvanSyniook@gmail.com>
2025-06-09 17:29:40 +0300

    Add button to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/shared/components";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
       <div className="flex flex-col items-center space-y-4">
         <h1 className="text-2xl font-bold">Hello it`s home page!!</h1>
+        <Button>Hello i`m not mafia</Button>
       </div>
     </div>
   );
