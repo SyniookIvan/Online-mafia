abffc2047e5e6929ede199b49d287fd7d6c3562a
Ivan <IvanSyniook@gmail.com>
2025-06-09 16:12:49 +0300

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex  justify-center">
-      <h1>
-        <b>Hello world.</b> It`s home page mafia.io
-      </h1>
+    <div className="flex min-h-screen justify-center items-center">
+      <div className="flex flex-col items-center space-y-4">
+        <h1 className="text-2xl font-bold">Hello it`s home page!!`</h1>
+      </div>
     </div>
   );
 }
